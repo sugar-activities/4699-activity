35ec8d8f5a59a91aefcb5c5648ffdd5ea0007cdf
Ignacio Rodriguez <ignacio@sugarlabs.org>
2017-04-21 01:33:12 -0400

    Initial commit

---
+++ lessons.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+from gettext import gettext as _
+import glib
+
+LESSONS = (
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lección 3')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Puntos clave:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • La Luz es la fuente de todo lo que existe en este mundo.')) + \
+        '\n' + \
+        glib.markup_escape_text(_("   • La naturaleza de la Luz es compartir siempre.")) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • La Luz es la fuente de toda la plenitud espiritual.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • La Luz siempre está accesible para nosotros.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Palabras nuevas:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('Luz: La fuente de todo lo que existe en este mundo; una fuerza constante de compartir que nos brinda completa e ilimitada felicidad y plenitud espiritual.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Reflexiona')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Describe un momento en el que sentiste la Luz en tu vida; los momentos que te sentiste feliz, libre o emocionado.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Describe un momento en el que no sentiste la Luz en tu vida; los momentos en los que te sentiste triste, decepcionado o enojado.')),
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lección 3')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Puntos clave:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • La Luz es la fuente de todo lo que existe en este mundo.')) + \
+        '\n' + \
+        glib.markup_escape_text(_("   • La naturaleza de la Luz es compartir siempre.")) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • La Luz es la fuente de toda la plenitud espiritual.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • La Luz siempre está accesible para nosotros.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Palabras nuevas:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('Luz: La fuente de todo lo que existe en este mundo; una fuerza constante de compartir que nos brinda completa e ilimitada felicidad y plenitud espiritual.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Reflexiona')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Describe un momento en el que sentiste la Luz en tu vida; los momentos que te sentiste feliz, libre o emocionado.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Describe un momento en el que no sentiste la Luz en tu vida; los momentos en los que te sentiste triste, decepcionado o enojado.')),
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lección 4')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Regla #3: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('Todos tenemos una chispa de Luz adentro.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Puntos clave:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Cada uno de nosotros tiene una chispa de Luz adentro.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Nuestra esencia es bondad.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Dentro de cada uno de nosotros está el tremendo poder de la Luz.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Cada uno de nosotros es especial y único.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Reflexiona')) + \
+        '</span>' + \
+        '\n\n' + \
+         glib.markup_escape_text(_('   • Piensa en un momento en el que estabas expresando tus cualidades de la Luz.  ¿Que pasó? ¿Cómo te sentiste?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Piensa en un momento en el que no estabas expresando tus cualidades de la Luz.  ¿Qué pasó? ¿Cómo te sentiste?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • ¿Siempre recuerdas que todos tenemos Luz dentro? ¿Por que sí o por que no?')),
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lección 4')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Regla #3: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('Todos tenemos una chispa de Luz adentro.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Puntos clave:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Cada uno de nosotros tiene una chispa de Luz adentro.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Nuestra esencia es bondad.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Dentro de cada uno de nosotros está el tremendo poder de la Luz.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Cada uno de nosotros es especial y único.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Reflexiona')) + \
+        '</span>' + \
+        '\n\n' + \
+         glib.markup_escape_text(_('   • Piensa en un momento en el que estabas expresando tus cualidades de la Luz.  ¿Que pasó? ¿Cómo te sentiste?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Piensa en un momento en el que no estabas expresando tus cualidades de la Luz.  ¿Qué pasó? ¿Cómo te sentiste?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • ¿Siempre recuerdas que todos tenemos Luz dentro? ¿Por que sí o por que no?')),
+
+)
---
+++ videos.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+VIDEOS = (
+    ("3.1.ogv",
+     _('La Luz'),
+     _('La plenitud espiritual es nuestra meta en la vida, pero a veces, nuestros hijos pierden el rumbo en el logro de ese objetivo. Pueden sentirse perdidos, asustados o frustrados - como con nubes oscuras pesando sobre sus cabezas. En esta lección, nos enteramos que así como el sol siempre está brillando detrás de las nubes oscuras, de esa misma manera la Luz - la fuente de la positividad, la fuerza, la claridad y la seguridad – está siempre disponible para nosotros.'),
+      ),
+    ("3.2.ogv",
+     _('El Poder Invisible'),
+     '',
+      ),
+    ("4.ogv",
+     _('Soy un Jugador Clave'),
+     _('¡No puedo hacerlo!" "¡Soy un bueno para nada!" "¡Soy tan tonto!" A menudo escuchamos a nuestros hijos pronunciar estas frases de frustración cuando se sienten desanimados, dudosos de sí mismos y dispuestos a darse por vencidos. Con esta lección, van a empezar a reconocer su propia Luz, su autoestima, bondad innata y habilidades únicas.'),
+     ),
+    ("4.1.ogv",
+     _('La Única Cosa'),
+     '',
+     ),
+)
